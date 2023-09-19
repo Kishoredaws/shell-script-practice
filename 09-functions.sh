851448ae1a9e30f13c6d0ab4a81cadc643b9a91d
Kishore <kishore@ficha.fr>
2023-09-20 04:36:35 +0530

    my ninth script

---
+++ 09-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+VALIDATE() {
+if [ $1 -ne 0 ]
+then 
+echo " $2 Installation of Mysql is error"
+exit 1
+else
+echo "$2 Installation of Mysql is Success"
+fi 
+
+}
+if [ $USER_ID -ne 0 ]
+then
+echo "ERROR: Please run your script with root user"
+exit 1
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing Mysql"
+yum install postfix -y 
+VALIDATE $? "Installaing Postfix"
+

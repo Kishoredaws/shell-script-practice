eaec17b64ae974c792522f8ea4feef659f0b8590
Kishore <kishore@ficha.fr>
2023-09-19 11:14:13 +0530

    my sixth script

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSONS=(Kishore, Supriya, Sahasra, Sanvita, Shankaraiah, Hemalatha)
+
+echo "The first person Name is : ${PERSONS[0]}"
+echo "The Second person Name is : ${PERSONS[1]}"
+echo "The Third person Name is : ${PERSONS[2]}"
+echo "The Fourth person Name is : ${PERSONS[3]}"
+echo "The Fifth person Name is : ${PERSONS[4]}"
+echo "The Sixth person Name is : ${PERSONS[5]}"
+echo "All Persons names are : ${PERSONS[@]}"
+

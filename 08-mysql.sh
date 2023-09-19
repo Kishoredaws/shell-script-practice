c1b36248c8854c72b8e6192a4d920219d256f490
Kishore <kishore@ficha.fr>
2023-09-19 11:48:11 +0530

    my eighth script

---
+++ 08-mysql.sh
@@ -0,0 +1,20 @@
+# Installation of mysql into Centos
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]
+then
+echo "ERROR: Please run your script with root user"
+exit 1
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+echo "ERROR: Installation of Mysql is error"
+exit 1
+else
+echo "Installation of Mysql is Success"
+fi 

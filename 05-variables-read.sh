ab2203ddab513881e919c74f6705d60e6a01dc84
Kishore <kishore@ficha.fr>
2023-09-19 11:05:16 +0530

    my fifth script

---
+++ 05-variables-read.sh
@@ -0,0 +1,7 @@
+echo "Please enter the Username"
+read -s USERNAME
+echo "you have entered the username is : $USERNAME"
+
+echo "Please enter your Password"
+read -s PASSWORD
+echo "You have entered the Password is : $PASSWORD"

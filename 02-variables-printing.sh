d683bddfedd9a6050c887991bdb4a89d351b1af8
Kishore <kishore@ficha.fr>
2023-09-19 05:36:08 +0530

    my second script

---
+++ 02-variables-printing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# PERSON1=Kishore
+# PERSON2=Supriya
+
+echo "Kishore: Hai Supriya, How are you?"
+echo "Supriya: Hai Kishore, I am good and what about you Kishore"
+echo "Kishore: Yeah! I am also good and what are you doing Supriya"
+echo "Supriya: Yes Kishore I am working as a Tecnical recruiter Kishore"

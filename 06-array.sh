31e31a2a5ae6bfde9c19a17bb4eac4e88890e34a
Kishore <kishore@ficha.fr>
2023-09-19 11:17:38 +0530

    my sixth script

--- 06-array.sh
+++ 06-array.sh
@@ -7,6 +7,6 @@ echo "The Third person Name is : ${PERSONS[2]}"
 echo "The Fourth person Name is : ${PERSONS[3]}"
 echo "The Fifth person Name is : ${PERSONS[4]}"
 echo "The Sixth person Name is : ${PERSONS[5]}"
-echo "The Seventh Person Name is: ${PERSONS[6]}"
+echo "The first and third persons names are: ${PERSONS[0,2]}
 echo "All Persons names are : ${PERSONS[@]}"
 

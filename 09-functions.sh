54f02c212cd378f2ba48e1573dec700a45e66661
Kishore <kishore@ficha.fr>
2023-09-20 04:48:03 +0530

    my ninth script

--- 09-functions.sh
+++ 09-functions.sh
@@ -2,16 +2,17 @@
 
 USER_ID=$(id -u)
 
-VALIDATE() {
-if [ $1 -ne 0 ]
-then 
-echo " $2...... Failure"
-exit 1
-else
-echo "$2 ....... Success"
-fi 
-
+VALIDATE(){
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
+
 if [ $USER_ID -ne 0 ]
 then
 echo "ERROR: Please run your script with root user"

72d6b029f9e4bc0aac24940f506ca583d2d9cb0d
Kishore <kishore@ficha.fr>
2023-09-21 05:10:26 +0530

    my 12th script

--- 12-loops.sh
+++ 12-loops.sh
@@ -4,12 +4,13 @@ N="\e[0m
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ];
-then    
-    echo -e " $R ERROR:: please run this script with root access $N "
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
-fi 
+fi
 
 for i in $@
 do
 yum install $i -y 
 done
+

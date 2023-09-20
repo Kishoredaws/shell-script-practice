ab50cd6b9c67fc558aac9b77960c90f054831553
Kishore <kishore@ficha.fr>
2023-09-20 05:30:41 +0530

    my ninth script

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,7 +4,6 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=tmp/$SCRIPT_NAME-$DATE.log
 
-
 VALIDATE(){
     #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]
@@ -15,7 +14,9 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
 USER_ID=$(id -u)
+
 if [ $USER_ID -ne 0 ]
 then
 echo "ERROR: Please run your script with root user"
@@ -26,4 +27,3 @@ yum install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing Mysql" 
 yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing Postfix"
-

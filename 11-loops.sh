0488eabbea45c3ad9cb18be20ec930b8ad81e001
Kishore <kishore@ficha.fr>
2023-09-21 04:08:52 +0530

    my 11th script

--- 11-loops.sh
+++ 11-loops.sh
@@ -3,7 +3,36 @@ DATE=$(date +f)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 for i in $@
 do
     yum install $i -y &>>$LOGFILE
 done
+
+VALIDATE(){
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+
+VALIDATE $? "Installing MySQL"
+VALIDATE $? "Installing Postfix"
+VALIDATE $? "Installing git"

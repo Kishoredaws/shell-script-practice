1ccbb959877454a1ee479ac47766ef081bd15507
Kishore <kishore@ficha.fr>
2023-09-20 05:47:41 +0530

    my ninth script

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# our program goal is to install mysql
+
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-LOGFILE=tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+# this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
-
+    #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... FAILURE"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -19,13 +26,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-echo "ERROR: Please run your script with root user"
-exit 1
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
 fi
+
+# it is our responsibility again to check installation is success or not
 yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
-yum install postfix -y &>>$LOGFILE
+yum install posddtfix -y &>>$LOGFILE
 
 VALIDATE $? "Installing postfix"

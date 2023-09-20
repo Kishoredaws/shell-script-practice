30eef36cb14338bf1b5dbf27b057ccc0884ae8a6
Kishore <kishore@ficha.fr>
2023-09-20 05:38:14 +0530

    my ninth script

--- 09-functions.sh
+++ 09-functions.sh
@@ -5,7 +5,7 @@ SCRIPT_NAME=$0
 LOGFILE=tmp/$SCRIPT_NAME-$DATE.log
 
 VALIDATE(){
-    #$1 --> it will receive the argument1
+
     if [ $1 -ne 0 ]
     then
         echo -e "$2 ... $R FAILURE $N"
@@ -15,9 +15,9 @@ VALIDATE(){
     fi
 }
 
-USER_ID=$(id -u)
+USERID=$(id -u)
 
-if [ $USER_ID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
 echo "ERROR: Please run your script with root user"
 exit 1

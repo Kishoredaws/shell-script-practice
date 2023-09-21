1d0cbd2808d7d6f240a47234bdee17aaeb3363c5
Kishore <kishore@ficha.fr>
2023-09-21 11:26:40 +0530

    my 12th script

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,46 +1,42 @@
-# To install multiple packages with single script
-#step1: user should have root access
-#step2: While installing store the logs
-#step3: Implement colors for user experience
-#step4: Before installing of package it always to check the package installed or not
-#step5: If installed skip otherwise poceed for installations
-#step6: finally Check if the packages installed successfully or not?
-
-# R="\e[31m"
-# N="\e[0m"
-# G="\e[32m"
-# Y="\e[33m"
-
 DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGDIR=/home/centos/shellscript-logs
-LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
-
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
 then
-    echo "ERROR: Please run the scrip with root access"
-exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 fi
 
 VALIDATE(){
-    if [$1 -ne 0 ]
+    if [ $1 -ne 0 ];
     then
-        echo "$2 The installation is Failure"
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
     else
-        echo "$2 the installation is Success"
-    fi 
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
 }
 
+# all args are in $@
 for i in $@
 do
-yum installed list $i >>$LOGFILE
-if [ $? -ne 0 ]
-then 
-    echo "$i is not installed lets install it"
-    yum install $i -y
-    VALODATE $? "$i"
-else
-    echo "$i is already installed"
-fi 
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+
+    #yum install $i -y
 done

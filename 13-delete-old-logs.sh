daab78adedd426c7d6d88820ae7a13de555a3f96
Kishore <kishore@ficha.fr>
2023-09-22 05:24:24 +0530

    my 13th script

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -2,7 +2,8 @@
 LOG_FILES_DIR=/home/centos/app-logs
 DATE=$(date -u)
 SCRIPT_NAME=$0
-LOGFILE=$LOG_FILES_DIR-$SCRIPT_NAME-$DATE.log
+LOGDIR=/home/centos/shellscript-logs
+LOGFILE=$LOGDIR-$SCRIPT_NAME-$DATE.log
 
 DELETE_OLD_FILES=$(find $LOG_FILES_DIR -name "*.log" -type f -mtime +14)
 

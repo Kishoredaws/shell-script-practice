2574ba6b95e1128d7ee8ad895e0561ff72c63b0a
Kishore <kishore@ficha.fr>
2023-09-22 11:25:14 +0530

    my 12th script

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-APP_LOGS_DIR=/home/centos/app-logs
-DATE=$(date +%F:%H:%M:%S)
-SCRIPT_NAME=$0
-LOGDIR=/home/centos/shellscript-logs
-LOGFILE=$LOGDIR-$SCRIPT_NAME-$DATE.log
 
-DELETE_OLD_FILES=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F:%H:%M:%S)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
 echo "script started executing at $DATE" &>>$LOGFILE
 
+while read line
+do
+    echo "Deleting $line" &>>$LOGFILE
+    rm -rf $line
+done <<< $FILES_TO_DELETE
 
 

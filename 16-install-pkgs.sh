9fde8f2d6db2d3bfc68029843ff9a40bd54e50fb
Kishore <kishore@ficha.fr>
2023-09-23 20:45:08 +0530

    my 16th script

--- 16-install-pkgs.sh
+++ 16-install-pkgs.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR: Please run the script with root access"
+    echo "ERROR:: Please run the script with root access"
 exit 1
 fi
 
 VALIDATE(){
-
-if [ $1 -ne 0 ];
-then
-    echo "Installing $2 FAILURE "
-    exit 1
-else
-    echo "Installing $2 SUCCESS"
-fi 
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 installation is FAILURE"
+    else
+        echo "$2 installation is SUCCESS"
+    fi
 }
 
 for i in $@
@@ -22,11 +21,12 @@ do
 yum list installed $i
 if [ $? -ne 0 ]
 then
-    echo "$i is not installed, lets install it"
+    echo "The $i is not installed, so lets install it"
     yum install $i -y
     VALIDATE $? "$i"
+    exit 1
 else
-    echo "$i is already installed"
+    echo "The $i is already installed"
 fi
 done
 
